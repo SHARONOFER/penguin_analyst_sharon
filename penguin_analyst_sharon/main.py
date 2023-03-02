9f6808db9c70c18ee0daff9f58b7d2b3d4a1cb50
SHARONOFER <35968217+SHARONOFER@users.noreply.github.com>
2023-03-02 19:10:58 +0200

    Initial commit

---
+++ penguin_analyst_sharon/main.py
@@ -0,0 +1,12 @@
+# print ('hi')
+import pandas
+import pandas as pd
+
+pd
+
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/penguins.csv"
+)
+
+print("    5   ")
+print(len(df), "       fff")
